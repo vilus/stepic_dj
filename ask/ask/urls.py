1f60b9598cf77631e4978eae411910e25e1ef19e
vilus <vilus@example.com>
2016-03-06 13:08:57 -0500

    stepic: module 2, lesson 1, fix 2

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from qa.views import test
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'.*', include('qa.urls')),
-    url(r'\/', include('qa.urls')),
-    url(r'login/', include('qa.urls')),
-    url(r'signup/', include('qa.urls')),
-    url(r'question/\d+/', include('qa.urls')),
-    url(r'ask/', include('qa.urls')),
-    url(r'popular/', include('qa.urls')),
-    url(r'new/', include('qa.urls')),
+    url(r'^$', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^question/\d+/', test),
+    url(r'^ask/', test),
+    url(r'^popular/', test),
+    url(r'^new/', test),
 ]
