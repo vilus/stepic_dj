eb37c02a8f5508db327773e283796aeb78c0a252
vilus <vilus@example.com>
2016-03-07 19:40:00 -0500

    lesson 2.3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=512)
+    text = models.TextField()
+    added_ad = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='for_likes')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
